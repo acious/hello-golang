4d0ba9e150421f4508b9194a263f1819443de2de
Jongchan Kim <ace.jckim@gmail.com>
2016-07-16 10:10:27 +0900

    delete comment

--- src/hellogo/crawler.go
+++ src/hellogo/crawler.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 )
 
-
-//기본이 되는 Go언어의 기본 소스 구조 작성
-func main(){
-	// 크롤링할 URL과 그 fetcher
-    Crawl("http://golang.org/", 4, fetcher) 
+func main() {
+	Crawl("http://golang.org/", 4, fetcher)
 }
